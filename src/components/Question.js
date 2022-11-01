f16a02a9226846b894a9ca2f9aee4bcc3d56b776
Kent Canonigo <kcanonigo@middlebury.edu>
2022-11-01 18:41:24 -0400

    Fixed merge conflict with question component and main

---
+++ src/components/Question.js
@@ -0,0 +1,42 @@
+/*
+  Question.js
+
+  The Question displays the contents of a quiz question. 
+
+  props:
+    question - the question to render (required) with properties learningGoal, question, choices, and answer
+    setCurrentAnswer - callback function to set the current answer of which answer option has been selected 
+*/
+
+import PropTypes from "prop-types";
+import QuestionShape from "./QuestionShape";
+// import { useState } from "react";
+
+export default function Question({ question, selectAnswer }) {
+  // Display the question title
+  const questionTitle = <div> {question.question} </div>;
+
+  // Display answer choices
+  const answerChoices = question.choices.map((choice) => {
+    return (
+      <li key={choice} id="choice" onClick={() => selectAnswer(choice)}>
+        {choice}
+      </li>
+    );
+  });
+
+  console.log(selectedAnswer);
+
+  return (
+    <div>
+      {questionTitle}
+      <ul>{answerChoices}</ul>
+    </div>
+  );
+}
+
+// Set question component prop types
+Question.propTypes = {
+  question: QuestionShape,
+  selectAnswer: PropTypes.func,
+};
