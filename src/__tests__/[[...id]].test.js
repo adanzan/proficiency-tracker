8a297a522b747205885a3514eb0462252d41bf14
adanzan <dachiterdene@middlebury.edu>
2022-11-03 14:54:07 -0400

    Fixed test error that was caused by renamed index page

--- src/__tests__/[[...id]].test.js
+++ src/__tests__/[[...id]].test.js
@@ -1,6 +1,6 @@
 import { render, screen } from "@testing-library/react";
 
-import Main from "../pages/index";
+import Main from "../pages/[[...id]]";
 
 /**
  * This is a placeholder test that is here only to make sure `npm test` will not choke because there are no tests. It should be removed as soon as you write a real test.
