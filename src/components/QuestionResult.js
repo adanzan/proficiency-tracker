88deace89ca74c349f3a0a41dc1b16481c3555f3
adanzan <dachiterdene@middlebury.edu>
2022-11-16 20:23:51 -0500

    Added comments, shortened functions

--- src/components/QuestionResult.js
+++ src/components/QuestionResult.js
@@ -1,43 +1,36 @@
 /*
-  Question.js
+  QuestionResult.js
 
-  The Question displays the contents of a quiz question. 
+  Displays a student attempt on a quiz
 
   props:
-    question - the question to render (required) with properties learningGoal, question, choices, and answer
-    selectAnswer - callback function to set the current answer of which answer option has been selected 
+    question - the question to render (required) with properties learningGoal, question and choices
+    studentAnswer - the answer selected by the student and whether it is correct
 */
 
 import PropTypes from "prop-types";
 import QuestionShape from "./QuestionShape";
+
 import styles from "../styles/QuestionResult.module.css";
 
 export default function QuestionResult({ question, studentAnswer }) {
-  // Display the question title
   const questionTitle = <div> {question.question} </div>;
 
-  // Display answer choices
+  // Formats the choices based on the student answer
   const answerChoices = question.choices.map((choice) => {
-    const fontweightStyle = {
-      fontWeight: choice === studentAnswer.answer ? "bold" : "lighter",
-    };
-    //const colorStyle = {color: choice === studentAnswer.answer? "green" : "red" }
+    const fontWeightStyle =
+      choice === studentAnswer.answer ? "bold" : "lighter";
 
     let colorStyle = "black";
-
     if (choice === studentAnswer.answer) {
-      if (studentAnswer.correct) {
-        colorStyle = "green";
-      } else {
-        colorStyle = "red";
-      }
+      colorStyle = studentAnswer.correct ? "green" : "red";
     }
 
     return (
       <li
         className={styles.li}
         key={choice}
-        style={{ ...fontweightStyle, color: colorStyle }}
+        style={{ fontWeight: fontWeightStyle, color: colorStyle }}
       >
         {choice}
       </li>
@@ -52,9 +45,7 @@ export default function QuestionResult({ question, studentAnswer }) {
   );
 }
 
-// Set question component prop types
 QuestionResult.propTypes = {
   question: QuestionShape,
-  selectAnswer: PropTypes.func,
-  studentAnswer: PropTypes.object,
+  studentAnswer: PropTypes.object.isRequired,
 };
