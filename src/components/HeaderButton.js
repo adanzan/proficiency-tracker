07e3ca398bc40143b3e45915310256b37053b007
Blair Jia <bjia@middlebury.edu>
2022-12-05 17:16:08 -0500

    fixed header styling and errors

--- src/components/HeaderButton.js
+++ src/components/HeaderButton.js
@@ -3,15 +3,18 @@ import { useUser } from "../contexts/UserContext";
 import Button from "react-bootstrap/Button";
 import "bootstrap/dist/css/bootstrap.css";
 // import styles from "../styles/headerButton.css";
+import { useRouter } from "next/router";
 
 export default function HeaderButton() {
   const user = useUser();
+  const router = useRouter();
 
   return (
     <div>
-      <span>
+      <span className="position-absolute top-0 start-0">
         <Button onClick={() => router.push("/")}>Home</Button>
-
+      </span>
+      <span className="position-absolute top-0 end-0">
         <Button onClick={() => router.push("/")}>
           {user ? user.email : "Log in"}
         </Button>
